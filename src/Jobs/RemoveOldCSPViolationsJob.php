5340175117749c2f4450ba03ba839baffb5d4ef0
Tom St George <stgeorge.t@gmail.com>
2026-04-18 16:27:09 -0500

    fix: restore job logic and properly implement SS6 PolyCommand API for BuildTasks

--- src/Jobs/RemoveOldCSPViolationsJob.php
+++ src/Jobs/RemoveOldCSPViolationsJob.php
@@ -10,38 +10,82 @@ use SilverStripe\ORM\DB;
 use SilverStripe\ORM\DataList;
 use Symbiote\QueuedJobs\Services\AbstractQueuedJob;
 use Symbiote\QueuedJobs\Services\QueuedJobService;
-use Signify\SecurityHeaders\Tasks\RemoveOldCSPViolationsTask;
-use SilverStripe\PolyExecution\PolyOutput;
-use Symfony\Component\Console\Output\NullOutput;
 
 /**
  * A queued job to remove old CSPViolation objects.
  */
 class RemoveOldCSPViolationsJob extends AbstractQueuedJob
 {
-    public function __construct()
-    {
-        $this->totalSteps = CSPViolation::get()->count();
-    }
+    /**
+     * Reports that were last changed longer than this period ago can be deleted.
+     *
+     * The string must be in DateInterval duration format.
+     * @see https://www.php.net/manual/en/dateinterval.construct.php
+     *
+     * @var string
+     */
+    private static $retention_period = 'P1M';
 
-    public function getTitle()
+    public function setup()
     {
-        return _t(
-            __CLASS__ . '.TITLE',
-            'Remove old CSP violations'
-        );
-    }
+        $retention = Config::inst()->get(self::class, 'retention_period');
+        $retention = new DateInterval($retention);
 
-    public function getJobType()
-    {
-        return \Symbiote\QueuedJobs\Services\QueuedJob::QUEUED;
+        $date = new DateTime();
+        $date->sub($retention);
+        $this->retentionDate = $date->format(DateTime::ATOM);
+
+        $this->reportsDeleted = 0;
+
+        $this->totalSteps = $this->getItemsList()->count();
     }
 
     public function process()
     {
-        $task = new RemoveOldCSPViolationsTask();
-        $output = PolyOutput::create(PolyOutput::FORMAT_ANSI, new NullOutput());
-        $task->run($output, $output);
-        $this->isComplete = true;
+        $batchSize = Config::inst()->get(CSPViolationsReport::class, 'deletion_batch_size');
+
+        $oldReports = $this->getItemsList()->limit($batchSize);
+
+        $delta = 0;
+
+        // Wrapped in a transaction for performance only.
+        try {
+            DB::get_conn()->transactionStart();
+
+            /** @var CSPViolation $report */
+            foreach ($oldReports as $report) {
+                # See https://github.com/silverstripe/silverstripe-framework/issues/1903
+                $report->Documents()->removeAll();
+                $report->delete();
+                $delta++;
+            }
+        }
+        finally {
+            DB::get_conn()->transactionEnd();
+        }
+
+        $this->reportsDeleted += $delta;
+        $this->currentStep += $delta;
+
+        if ($delta < $batchSize) {
+            $this->isComplete = true;
+            print 'Removed ' . number_format($this->reportsDeleted) . ' reports.' . "\n";
+
+            $deletionJob = new RemoveUnreferencedCSPDocumentJob();
+            $jobId = singleton(QueuedJobService::class)->queueJob($deletionJob);
+
+            print "Unreferenced CSP Document job queued with ID $jobId\n";
+
+        }
+    }
+
+    public function getTitle()
+    {
+        return 'Remove old CSP Violation reports';
+    }
+
+    private function getItemsList(): DataList
+    {
+        return CSPViolation::get()->filter(['ReportedTime:LessThan' => $this->retentionDate]);
     }
 }
--- src/Tasks/RemoveUnreferencedCSPDocumentsTask.php
+++ src/Tasks/RemoveUnreferencedCSPDocumentsTask.php
@@ -3,33 +3,32 @@
 namespace Signify\SecurityHeaders\Tasks;
 
 use Signify\SecurityHeaders\Jobs\RemoveUnreferencedCSPDocumentJob;
-use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Dev\BuildTask;
 use SilverStripe\PolyExecution\PolyOutput;
 use Symbiote\QueuedJobs\Services\QueuedJobService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
 
 class RemoveUnreferencedCSPDocumentsTask extends BuildTask
 {
     protected string $title = 'Remove unreferenced CSP Document URIs';
 
-    protected string $description =
+    protected static string $commandName = 'remove-unreferenced-csp-documents';
+
+    protected static string $description =
     'CSP Document URIs that are not referenced by a CSP violation report can be safely removed.';
 
-    /**
-     * {@inheritDoc}
-     * @see \SilverStripe\Dev\BuildTask::run()
-     */
-    public function run(HTTPRequest|PolyOutput $request, PolyOutput|null $output = null): bool
+    protected function execute(InputInterface $input, PolyOutput $output): int
     {
         $deletionJob = new RemoveUnreferencedCSPDocumentJob();
 
         $jobId = singleton(QueuedJobService::class)->queueJob($deletionJob);
 
-        $output?->writeln("Job queued with ID $jobId");
-        return true;
+        $output->writeln("Job queued with ID $jobId");
+        return Command::SUCCESS;
     }
 
-    public function isEnabled()
+    public function isEnabled(): bool
     {
         return parent::isEnabled() && class_exists(QueuedJobService::class);
     }
